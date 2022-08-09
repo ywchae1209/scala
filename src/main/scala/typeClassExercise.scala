fd7befb9dbf083efcafa2923e269a890486f0fb5
ywchae1209 <path0s@naver.com>
2022-08-09 13:46:33 +0900

    Type class exercise..

---
+++ src/main/scala/typeClassExercise.scala
@@ -0,0 +1,74 @@
+
+object typeClassExercise {
+
+  ////////////////////////////////////////////////////////////////////////////////
+  trait StringParser[T] { def parse(s: String) : T }
+
+  object StringParser {
+    implicit object ParseSt extends StringParser[String] {
+      override def parse(s: String): String = s
+    }
+    implicit object ParseBoolean extends StringParser[Boolean] {
+      override def parse(s: String): Boolean = s.toBoolean
+    }
+    implicit object ParseDouble extends StringParser[Double] {
+      override def parse(s: String): Double = s.toDouble
+    }
+    implicit object ParseInt extends StringParser[Int] {
+      override def parse(s: String): Int = s.toInt
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////
+    //
+    //    [T: StringParser] ....
+    //    val p = implicitly[StringParser[T]]
+    //
+    //    is equal to
+    //
+    //    [T](implicit p: StringParser[T]kj
+    //
+    ////////////////////////////////////////////////////////////////////////////////
+
+    implicit def ParseSeq[T: StringParser]: StringParser[Seq[T]] =
+      (s: String) => {
+        val p = implicitly[StringParser[T]]
+
+        println(s"\tSeq of $s")
+        s.split(",").toSeq.map(p.parse)
+      }
+
+    implicit def parseTuple[T, V](implicit p1: StringParser[T], p2 : StringParser[V])
+    : StringParser[(T,V)] =
+      s => {
+//        val Array( lhs, rhs) = s.split("=")
+
+        val n = s.indexOf("=")
+        val lhs = s.take(n)
+        val rhs = s.drop(n+1)
+
+        println( s"\tlhs: $lhs, rhs: $rhs")
+        p1.parse(lhs) -> p2.parse(rhs)
+      }
+
+  }
+
+  ////////////////////////////////////////////////////////////////////////////////
+
+  def parseString[T: StringParser](s: String): T =
+    implicitly[StringParser[T]].parse(s)
+
+  def main(args: Array[String]): Unit = {
+    println { parseString[Boolean]("true") }
+    println { parseString[Seq[Boolean]]("true,false,true") }
+    println { parseString[(String, Int)]("x=123") }
+    println { parseString[(String, Seq[Int])]("x=123,1,2,3,4,5") }
+    println { parseString[(Seq[String], Seq[Int])]("x,y,z=123,1,2,3,4,5") }
+    println { parseString[Seq[(String, Int)]]("x=1,y=2,z=3") }
+
+    println { parseString[(String, (String, Int))]("x=y=123") }
+
+//    println { parseString[Seq[(Seq[String], Seq[Int])]]("a,b=1,2,c=2,d,e=3,4") }
+
+  }
+
+}
