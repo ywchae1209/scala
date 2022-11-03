1d5eca69c63a3376ba4424733d30212196cfa92a
ywchae1209 <110512765+ywchae1209@users.noreply.github.com>
2022-11-03 13:12:31 +0900

    Update Trampoline.scala

--- someExercise/somethingtoString/Trampoline.scala
+++ someExercise/somethingtoString/Trampoline.scala
@@ -1,50 +1,66 @@
 package recursion
 
+import scala.annotation.tailrec
+
 object Trampoline {
 
   sealed trait TailRec[A] {
-    def flatMap[B](f: A => TailRec[B]): TailRec[B] = FlatMap(this, f)
-    def map[B](f: A => B): TailRec[B] = flatMap( a => Just(f(a)))
-    def run: A = Trampoline.run(this)
-  }
+    final def flatMap[B](f: A => TailRec[B]): TailRec[B] = FlatMap(this, f)
+    final def map[B](f: A => B): TailRec[B] = flatMap( a => Pure(f(a)))
+    final def run: A = Trampoline.run(this)
 
+    // evaluate a single layer
+    final def resume: Either[TailRec[A], A] = this match {
+      case Pure(a) => Right(a)
+      case Suspend(a) => a().resume
+      case x@FlatMap(_,_) => x.a match {
+        case Pure(a) => x.cont(a).resume
+        case Suspend(a) => a().flatMap(x.cont).resume
+        case y@FlatMap(_,_) => y.a.flatMap( a => y.cont(a).flatMap(x.cont)).resume
+      }
+    }
+  }
   def map2[A,B,C](a: TailRec[A], b: TailRec[B])(f: (A,B) => C)
   : TailRec[C] = a.flatMap(a0 => b.map(b0 => (f(a0, b0))))
 
   ////////////////////////////////////////////////////////////////////////////////
-  def pure[A](a : => A): TailRec[A] = Just(a)
+  def pure[A](a : => A): TailRec[A] = Pure(a)
   def suspend[A](a : => TailRec[A]): TailRec[A] = Suspend( () => a)
+  ////////////////////////////////////////////////////////////////////////////////
+  /*
+   * Analogy to Free
+   *  type Trampoline[+A] = Free[Function0, A]
+   *       Pure    ::: A
+   *       Suspend ::: F[Free[F,A]]
+   *       FlatMap ::: Free[F,A], A => Free[F,B]
+   */
+  private case class Pure[A](a: A) extends TailRec[A]
+  private case class Suspend[A](stub: () => TailRec[A]) extends TailRec[A]
+  private case class FlatMap[A0, B]( a0: TailRec[A0], cont0: A0 => TailRec[B]) extends TailRec[B]
+  {
+    // walk-around for existential type erasure
+    type A = A0
+    val a: TailRec[A] = a0
+    val cont: A => TailRec[B] = cont0
+  }
 
   ////////////////////////////////////////////////////////////////////////////////
-  private case class Just[A](a: A) extends TailRec[A]
-  private case class Suspend[A](stub: () => TailRec[A]) extends TailRec[A]
-  private abstract case class FlatMap[B]() extends TailRec[B] {
-    type A                    // walk-around for existential type erasure
-    val a: TailRec[A]
-    val cont: A => TailRec[B]
+  @tailrec
+  def run[A](tailrec: TailRec[A]): A = tailrec.resume match {
+    case Left(t)  => run(t)
+    case Right(a) => a
   }
 
-  private object FlatMap {
-    def apply[A0,B]( a0: TailRec[A0], cont0: A0 => TailRec[B]) =
-      new FlatMap[B] {
-        type A = A0
-        val a = a0
-        val cont = cont0
-    }
-  }
-  ////////////////////////////////////////////////////
-  @scala.annotation.tailrec
-  def run[A](tailrec: TailRec[A]): A = tailrec match {
-    case Just(a)     => a
-    case Suspend(r)  => run(r())
-    case f@FlatMap() => f.a match {
-      case Just(a)     => run(f.cont(a))
-      case Suspend(s)  => run(s().flatMap (f.cont))
-      case g@FlatMap() => run( g.a.flatMap(a => g.cont(a).flatMap(f.cont)) )
+  ////////////////////////////////////////////////////////////////////////////////
+  trait TrampolineInstances {
+    implicit val trampolineMonad = new Monad[TailRec] {
+      override def pure[A](a: => A): TailRec[A] = Pure(a)
+      override def flatMap[A, B](fa: TailRec[A])(f: A => TailRec[B]): TailRec[B] = fa.flatMap(f)
     }
   }
 }
 
+////////////////////////////////////////////////////////////////////////////////////////////////////
 object TrampolineTest extends App {
 
   import Trampoline._
@@ -67,5 +83,4 @@ object TrampolineTest extends App {
 
   val ex0 = List.fill(100000)(1).foldLeft(pure(0))((b, a) => suspend( map2(b, pure(a))( _ + _)))
   println(ex0.run)
-
 }
